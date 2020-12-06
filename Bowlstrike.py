0155aabb775853f1936070d05663dd5d6edcd353
Vitor A. Cazella <vitor.cazella@gmail.com>
2020-12-07 00:01:52 +1300

    Creating Test Cases

--- Bowlstrike.py
+++ Bowlstrike.py
@@ -1,11 +1,11 @@
 import unittest
 class BowlingGame(object):
     def __init__(self):
-        self.throw= []
+        self.throws = []
         self.score= 0
 
     def throw(self,pins):
-        self.throw.append(pins)
+        self.throws.append(pins)
 
 
     def calculate_score(self):
@@ -14,11 +14,11 @@ class BowlingGame(object):
             if self.throws[ball]==10:
                 self.score +=10 + self.throws[ball+1] + self.throws[ball +2]
                 ball += 1
-            elif self.throw[ball] + self.throws[ball+1] == 10:
+            elif self.throws[ball] + self.throws[ball+1] == 10:
                 self.score += 10 + self.throws [ball +2]
                 ball +=2
             else:
-                self.score += self.throws[ball] + self.throw[ball + 1]
+                self.score += self.throws[ball] + self.throws[ball + 1]
                 ball += 2
  
 class BowlingGameTests(unittest.TestCase):
@@ -27,28 +27,32 @@ class BowlingGameTests(unittest.TestCase):
             game.throw(pins)
 
     def test_all_gutters(self):
+        print("test_all_gutters")
         game = BowlingGame()
-        self.throw_many(game, 20 ,0 )
+        self.throw_many(game, 20, 0)
         game.calculate_score()
-        self.assertEquals(game.score,0)
+        self.assertEqual(game.score,0)
 
     def test_perfect_game(self):
+        print("test_perfect_game")
         game = BowlingGame()
         self.throw_many(game, 12, 10)
         game.calculate_score()
-        self.assertEquals(game.score, 300)
+        self.assertEqual(game.score, 300)
 
-        def test_all_ones(self):
-            game = BowlingGame()
-            number_of_times = 20
-            pins = 1
-            self.throw_many(game, number_of_times, pins)
-            game.calculate_score()
-            self.assertEquals(game.score, 20)
+    def test_all_ones(self):
+        print("test_all_ones")
+        game = BowlingGame()
+        number_of_times = 20
+        pins = 1
+        self.throw_many(game, number_of_times, pins)
+        game.calculate_score()
+        self.assertEqual(game.score, 20)
 
 
 
-    def test_different_throws (self):
+    def test_different_throws(self):
+        print("test_different_throws")
         game = BowlingGame()
         game.throw(6)
         game.throw(0)
@@ -58,10 +62,11 @@ class BowlingGameTests(unittest.TestCase):
         for _ in range(15):
             game.throw(0)
         game.calculate_score()
-        self.assertEquals(game.score, 15)
+        self.assertEqual(game.score, 15)
 
 
     def test_for_spare(self):
+        print("test_for_spare")
         game = BowlingGame()
         game.throw(4)
         game.throw(6)
@@ -70,13 +75,17 @@ class BowlingGameTests(unittest.TestCase):
         for _ in range(16):
             game.throw(0)
         game.calculate_score()
-        self.assertEquals(game.score, 24)
+        self.assertEqual(game.score, 24)
 
     def test_for_strike(self):
-        game=BowlingGame()
+        print("test_for_strike")
+        game = BowlingGame()
         game.throw(10)
         game.throw(4)
         game.throw(2)
         self.throw_many(game, 17,0)
         game.calculate_score()
-        self.assertEquals(game.score, 22)
+        self.assertEqual(game.score, 22)
+
+if __name__ == '__main__':
+    unittest.main()
